6f8c5eec3462c2c9927963c2a4f26208883bb641
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-06-18 22:14:09 +0100

    Firebase Logout

--- app/src/main/java/com/example/authenticationwithfirebase/MainActivity.kt
+++ app/src/main/java/com/example/authenticationwithfirebase/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.authenticationwithfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.authenticationwithfirebase.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            if (firebaseAuth.currentUser == null){
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "Logout failed!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
     }
 }
